2c7c7c2cd5bd82571a713f8d229401dbfd778a99
Ralph <raftsdev@gmail.com>
2022-05-28 20:42:43 -0400

    Feat: Create new entry point class for lunch SwimCoach class.

---
+++ src/com/raftsdev/springdemo/SwimJavaConfigBeanExposeApp.java
@@ -0,0 +1,20 @@
+package com.raftsdev.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SwimJavaConfigBeanExposeApp {
+
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+		Coach theCoach = context.getBean("swimCoach", Coach.class);
+
+		System.out.println(theCoach.getDailyWorkout());
+
+		System.out.println(theCoach.getDailyFortune());
+
+		context.close();
+	}
+
+}
