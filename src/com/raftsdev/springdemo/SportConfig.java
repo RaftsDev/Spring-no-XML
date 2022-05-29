982ce40f740b740ea45d2a53f239464fe8e1d691
Ralph <raftsdev@gmail.com>
2022-05-28 20:29:34 -0400

    Feat: DEscribe a Beans in Configuration class.

--- src/com/raftsdev/springdemo/SportConfig.java
+++ src/com/raftsdev/springdemo/SportConfig.java
@@ -1,11 +1,22 @@
 package com.raftsdev.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.raftsdev.springdemo")
 public class SportConfig {
+	
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
 
 }
 
